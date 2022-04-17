252d736e7e1d994ee3a33dad53c476472f96db54
Fanni111 <33203547+Fanni111@users.noreply.github.com>
2022-04-17 20:27:46 +0200

    seperate into new component the expandable project item

---
+++ src/app/projectItem.ts
@@ -0,0 +1,15 @@
+
+export class ProjectItem {
+    imgSrc:string;
+    title:string;
+    category:string;
+    desc:string;
+
+    constructor( imgSrc:string,title:string, category:string, desc:string){
+            this.imgSrc = imgSrc;
+            this.title = title;
+            this.category = category;
+            this.desc = desc
+            ;
+        }
+  }
