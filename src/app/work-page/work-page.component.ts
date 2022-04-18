5423f29e277150c9ee6ebc87d55872eb58d95fd1
Fanni111 <33203547+Fanni111@users.noreply.github.com>
2022-04-18 10:11:06 +0200

    try to optimize work page comp problem with flexbox

--- src/app/work-page/work-page.component.ts
+++ src/app/work-page/work-page.component.ts
@@ -13,8 +13,8 @@ export class WorkPageComponent implements OnInit {
   keleszto: ProjectItem;
 
   constructor() { 
-    this.eskuvoKorzo = new ProjectItem("assets/images/eskuvokorzo_mockup.png","Velencei Esküvőkorzó", "Webdesign","A székesfehérvári királyi napok alatt nemzetközi táncosok hangulatos felvonulással, táncokkal és látványos gálával szórakoztatták a közönséget. A legszebb pillanatokról hangulatvideót készítettem.");
-    this.keleszto = new ProjectItem("assets/images/eskuvokorzo_mockup.png","Velencei Esküvőkorzó", "Webdesign","A székesfehérvári királyi napok alatt nemzetközi táncosok hangulatos felvonulással, táncokkal és látványos gálával szórakoztatták a közönséget. A legszebb pillanatokról hangulatvideót készítettem.");
+    this.eskuvoKorzo = new ProjectItem("assets/images/webdesign/eskuvokorzo_mockup.png","Velencei Esküvőkorzó", "Webdesign","A székesfehérvári királyi napok alatt nemzetközi táncosok hangulatos felvonulással, táncokkal és látványos gálával szórakoztatták a közönséget. A legszebb pillanatokról hangulatvideót készítettem.");
+    this.keleszto = new ProjectItem("assets/images/webdesign/keleszto_mockup.png","Kelesztő terasz", "Webdesign","Tavasszal a Kálvin tér közelében nyitotta meg teraszát a Kelesztő melynek kreatív arculatát a logótervezéstől, a kirakati táblákon át a honlap készítésig valósíthattam meg. Kellemes olaszos hangulatú kávézó/pizzéria, ízletes ételekkel és frissítő italokkal várja vendégeit melynek pompás kínálatába egy hangulatvideó elkészítésével biztosítottam betekintést.");
   }
 
   ngOnInit(): void {
