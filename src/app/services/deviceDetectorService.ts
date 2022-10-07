7e6e8bb135f8e309f74fb9eedd2c260c584ca65f
Fanni111 <fanni.borbas11@hotmail.hu>
2022-10-07 08:46:29 +0200

    add analytics, change browser tab icon

--- src/app/services/deviceDetectorService.ts
+++ src/app/services/deviceDetectorService.ts
@@ -8,11 +8,9 @@ export class DeviceDetectorService {
 isMobileDevice() {
   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
   // true for mobile device
-  console.log("mobile device");
   return true;
 }else{
   // false for not mobile device
-  console.log("not mobile device");
   return false;
 }
 }
